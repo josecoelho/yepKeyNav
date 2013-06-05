bd553a887f2ea5c689651e2ca0b9a5ced2ce4346
José Coelho <contato@josecoelho.com>
2013-06-05 17:57:45 -0300

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,64 @@ yepKeyNav
 =========
 
 JQuery plugin provides a way to navigate or select items on a page using keyboad shortcuts.
+
+= Dependencies
+ https://github.com/tzuryby/jquery.hotkeys/ 
+ and http://flesler.blogspot.com/2007/10/jqueryscrollto.html
+
+
+= USAGE
+
+    $("selector of a list").yepNavKey()
+
+all itens with class yep-key-nav are navigable
+
+example to return the jquery selector for all selected itens:
+    $("selector of a list").yepNavKey('selected', function(ret) {
+        ret.each(function() {
+                //do something
+        });
+    });
+
+== Events
+
+current.yepKeyNav - when the current item is changed
+lostCurrent.yepKeyNav - when item lost the current selecion
+select.yepKeyNav - when an item is selected
+unselect.yepKeyNav - when an item is unselected
+
+Example:
+
+    $("list").on('current.yepKeyNav', function(evt) {
+        $current = $(event.target)
+
+        #... do someting with new $current item
+    })
+
+
+== Default Options
+    classes: {
+        navigable: "yep-key-nav",
+        currentItem: "yep-key-nav-current",
+        selectedItem: "yep-key-nav-selected",
+        itemSelector: "yep-key-nav-selector", //should be a checkbox
+        selectAll: "yep-key-nav-select-all" //should be a checkbox, when cliecked all itens will be selected or unselected
+    },
+    scrollSelector: 'window', //selector of panel with scroll of items
+    shortcuts: {
+        next: "j",
+        prev: "k",
+        toggleSelect: "x"
+    }
+
+
+= Shortcuts
+
+  j -> next element
+  k -> previous element
+  x -> select current element
+  space -> exec a configured function
+
+= BUGS
+
+- navigable itens are found on entire document, and not only on $(this) scope
---
+++ yepKeyNav.js
@@ -0,0 +1,194 @@
+/*
+ *  Project: YepKeyNav
+ *  Version: 1.0
+ *  Description: JQuery plugin provides a way to navigate or select items on a page using keyboad shortcuts.
+ *  Author: Yepdev - José Coelho
+ *  Dependencies: https://github.com/tzuryby/jquery.hotkeys/ and http://flesler.blogspot.com/2007/10/jqueryscrollto.html
+ *
+ *	plugin structure from: https://github.com/jquery-boilerplate/boilerplate with some modifications
+ */
+
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ($, window, document, undefined) {
+
+    $.widget('yep.yepKeyNav', {
+        options: {
+            classes: {
+                navigable: "yep-key-nav",
+                currentItem: "yep-key-nav-current",
+                selectedItem: "yep-key-nav-selected",
+                itemSelector: "yep-key-nav-selector", //should be a checkbox
+                selectAll: "yep-key-nav-select-all" //should be a checkbox, when cliecked all itens will be selected or unselected
+            },
+            scrollSelector: 'window',
+            shortcuts: {
+                next: "j",
+                prev: "k",
+                toggleSelect: "x"
+            }
+        },
+
+        _create: function () {
+            console.debug("yepKeyNav: init for "+$(this.element).attr('class'))
+            var self = this; //used to maintain class context on events
+
+
+            $(document).bind("keyup."+self.options.shortcuts.toggleSelect, $.proxy(self.toggleSelectByEvent,self));
+            $(document).bind("keyup."+self.options.shortcuts.next, $.proxy(self.next,self));
+            $(document).bind("keyup."+self.options.shortcuts.prev, $.proxy(self.prev,self));
+
+            $(document).on('click',"."+self.options.classes.navigable, $.proxy(self.currentByEvent,self));
+
+            $(document).on('click',"."+self.options.classes.itemSelector,$.proxy(self.toggleSelectByEvent,self));
+
+
+            $(document).on('click','.'+self.options.classes.selectAll, $.proxy(self.selectAll,self))
+
+            this.nav(0);
+        },
+        selectAll: function(evt) {
+            evt.stopPropagation()
+
+            var self = this
+            var $checkbox = $(evt.target);
+
+            var checked = $checkbox.is(':checked')
+
+
+            $all = $("."+this.options.classes.navigable)
+            $all.each(function(idx) {
+                if(checked) {
+                    self.select($(this))
+                } else {
+                    self.unselect($(this))
+                }
+            })
+
+        },
+        currentByEvent: function(evt) {
+            var $item = $(evt.target);
+
+            if(!$item.hasClass(this.options.classes.navigable)) {
+                $item = $item.parents("."+this.options.classes.navigable)
+            }
+
+            if(!$item.hasClass(this.options.classes.currentItem)) {
+                this.option('current',$item);
+            }
+
+        },
+        _currentChangeAndTriggerEvt: function($item) {
+            if(this.options.oldCurrent) {
+                this.options.oldCurrent.trigger('lostCurrent.yepKeyNav');
+            }
+
+
+            //remove from others
+            $("."+this.options.classes.currentItem).removeClass(this.options.classes.currentItem);
+            $item.addClass(this.options.classes.currentItem);
+
+            $(this.options.scrollSelector).scrollTo($item,200,{offset:-200});
+
+            $item.trigger('current.yepKeyNav');
+
+            this.options.current = $item;
+            this.options.oldCurrent = $item;
+
+
+
+
+            return $item;
+        },
+
+        _setOption: function(key,value) {
+            switch(key) {
+                case 'current':
+                    this._currentChangeAndTriggerEvt(value);
+            }
+        },
+
+        //navigate on elements, number can be positive or negative
+        nav: function(number) {
+            var $all, $current, $new_current, current_position, next_position = 0;
+
+            $all = $("."+this.options.classes.navigable)
+            if(!$all.length) {
+                console.debug("yepKeyNav: There's no items to nav")
+            } else {
+                $current = this.option('current')
+
+                current_position = $current ? $all.index($current) : 0;
+                next_position = current_position+number;
+
+                if(next_position >= $all.length) {
+                    next_position = 0;
+                } else if(next_position < 0) {
+                    next_position = $all.length-1
+                }
+
+                $new_current = $($all[next_position]);
+
+                this.option('current',$new_current)
+            }
+
+        },
+        next: function () {
+           this.nav(1)
+        },
+        prev: function() {
+           this.nav(-1)
+        },
+        toggleSelectByEvent: function(evt) {
+            $item = $(evt.currentTarget);
+
+            //currentTarget is a selector? then should select the navigable item of this selector
+            if($item.hasClass(this.options.classes.itemSelector)) {
+                this.toggleSelect($item.parents("."+this.options.classes.navigable));
+            } else { //otherwise, should select current navigable item
+                this.toggleSelect();
+            }
+
+        },
+        select: function($item) {
+            console.debug("yepKeyNav: adding selection");
+            $item.addClass(this.options.classes.selectedItem);
+
+            var $selector = $item.find("."+this.options.classes.itemSelector)
+
+            $selector.prop("checked",true);
+
+            $item.trigger('select.yepKeyNav');
+        },
+        unselect: function($item) {
+            console.debug("yepKeyNav: removing selection");
+            $item.removeClass(this.options.classes.selectedItem);
+
+            var $selector = $item.find("."+this.options.classes.itemSelector)
+
+            $selector.prop("checked",false);
+
+            $item.trigger('unselect.yepKeyNav');
+        },
+        toggleSelect: function($item) {
+
+            if(!$item) {
+                $item = this.options.current;
+            }
+
+
+
+            if($item.hasClass(this.options.classes.selectedItem)) {
+                this.unselect($item)
+            } else {
+                this.select($item)
+            }
+
+        },
+        selected: function(callback) {
+            callback( $("."+this.options.classes.selectedItem) );
+        }
+    });
+
+
+})(jQuery, window, document);
